e97d1f353cffd192d5872b9bfb56777fe3c5c98b
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-03-15 00:18:10 +0900

    3

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
-import tsconfigPaths from "vite-tsconfig-paths"
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/fe-socket-test',
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
   build: {
 	  assetsInlineLimit: 0,
   }
