baaf9e1bebcd59f7167a55bb7219586f41a4c7d1
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2023-03-15 18:33:13 +0900

    create ssl info

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,58 @@
-import React, { useRef, useEffect } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import io from 'socket.io-client';
+import type { Socket } from 'socket.io-client';
+import Webcam from 'react-webcam';
 
-export default function App() {
-  const videoRef = useRef<HTMLVideoElement>(null);
+export default function App(){
+  const [streamStarted, setStreamStarted] = useState<boolean>(false);
+  const [socket, setSocket] = useState<Socket>();
+  const webcamRef = useRef<Webcam>(null);
 
   useEffect(() => {
-    const socket = io('http://localhost:3001');
-
-    socket.on('frame', (data) => {
-      const blob = new Blob([data], { type: 'video/mp4' });
-      const url = URL.createObjectURL(blob);
-      if (videoRef.current) {
-        videoRef.current.src = url;
-      }
+    const newSocket = io('https://localhost:3001/camera', {
+      secure: true, // SSL/TLS 사용
+      rejectUnauthorized: false, // 인증서 오류 무시
     });
-
+    setSocket(newSocket);
     return () => {
-      socket.disconnect();
+      newSocket.disconnect();
     };
   }, []);
 
+  useEffect(() => {
+    if (socket) {
+      socket.on('streamStarted', (streamPath: string) => {
+        if (streamPath === 'live/stream') {
+          setStreamStarted(true);
+        }
+      });
+
+      socket.on('disconnect', () => {
+        console.log('Disconnected from server');
+        setStreamStarted(false);
+      });
+    }
+  }, [socket]);
+
   return (
     <div>
-      <video ref={videoRef} autoPlay />
-      <p>qwewqe</p>
+      {!streamStarted && <div>Waiting for stream to start...</div>}
+      {streamStarted && (
+        <Webcam
+          audio={false}
+          mirrored={true}
+          ref={webcamRef}
+          style={{
+            width: '100%',
+            height: 'auto',
+          }}
+          videoConstraints={{
+            width: 1280,
+            height: 720,
+            facingMode: 'user',
+          }}
+        />
+      )}
     </div>
   );
 };
